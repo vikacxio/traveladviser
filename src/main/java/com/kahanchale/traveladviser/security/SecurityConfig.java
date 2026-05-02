5b5970cf68fbef3eaf953a23ca61784e148e8590
Vikas Kumar <vikacxio@Vikass-MacBook-Air.local>
2026-05-02 14:28:24 +0530

    cors fix

--- src/main/java/com/kahanchale/traveladviser/security/SecurityConfig.java
+++ src/main/java/com/kahanchale/traveladviser/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.kahanchale.traveladviser.service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,9 +35,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/auth/login", "/auth/register", "/auth/success").permitAll()
                         .anyRequest().authenticated()
                 )
